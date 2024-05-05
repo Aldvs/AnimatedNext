b2f952ee42235cfd6a47adb201bf87fd1bf68f49
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-05 13:22:07 +0300

    Initial Commit

---
+++ AnimatedButtons/AnimatedButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedButtonsApp.swift
+//  AnimatedButtons
+//
+//  Created by admin on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
