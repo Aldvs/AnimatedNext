d7cbcd9ac00d7718bcb37218ea4e7ad8f5ddd585
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-07 09:56:27 +0300

    setup button and views

--- AnimatedButtons/ContentView.swift
+++ AnimatedButtons/ContentView.swift
@@ -9,13 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        InfiniteScrollView()
+    }
+}
+
+struct InfiniteScrollView: View {
+    @State private var isVisible = false
+    
+    var body: some View {
+        VStack() {
+            HStack(spacing: 0) {
+                if !isVisible {
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .foregroundStyle(.red)
+                        .offset(x: -12.5)
+                        .transition(
+                            .move(edge: .leading)
+                            .combined(with: .scale)
+                            .combined(with: .opacity)
+                        )
+                }
+                if isVisible {
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .foregroundStyle(.tint)
+                        .transition(
+                            .move(edge: .trailing)
+                        )
+                }
+                if isVisible {
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .foregroundStyle(.tint)
+                        .transition(
+                            .move(edge: .trailing)
+                            .combined(with: .scale)
+                            .combined(with: .opacity)
+                        )
+                }
+            }
+            .frame(width: 50, height: 30)
+            .border(.purple, width: 2.0)
+            
+            Button(action: {
+                withAnimation {
+                    self.isVisible.toggle()
+                }
+            }) {
+                Text("Анимировать")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
-        .padding()
+        .animation(.smooth, value: isVisible)
     }
 }
 
