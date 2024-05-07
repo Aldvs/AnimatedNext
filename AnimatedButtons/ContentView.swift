9f8092f88ec2181654a73e1ceddba13ea7b7a8ca
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-07 20:54:53 +0300

    update

--- AnimatedButtons/ContentView.swift
+++ AnimatedButtons/ContentView.swift
@@ -11,31 +11,28 @@ struct ContentView: View {
     @State private var isClicked = false
     var body: some View {
         VStack{
-            button
             Button(action: {
-                withAnimation{
+                withAnimation(Animation.smooth(duration: 0.5, extraBounce: 0.5)){
                     isClicked = true
                 }
             completion:{
                 isClicked = false
             }
-            }, label: {
+            }) {
                 HStack(spacing: .zero) {
                     Image(systemName: "play.fill")
                         .resizable()
                         .frame(width: isClicked ? 50 : 0, height: isClicked ? 50 : 0)
                         .opacity(isClicked ? 1 : 0)
-                    
                     Image(systemName: "play.fill")
                         .resizable()
                         .frame(width: 50, height: 50)
-                    
                     Image(systemName: "play.fill")
                         .resizable()
                         .frame(width: isClicked ? 1 : 50, height: isClicked ? 1 : 50)
                         .opacity(isClicked ? 0 : 1)
                 }
-            })
+            }
         }
     }
 }
